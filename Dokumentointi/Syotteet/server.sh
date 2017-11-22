f4f494f3aa7d0d4857e2a9c7981147add07fdd65
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-22 15:51:54 +0200

    Create server.sh

---
+++ Dokumentointi/Syotteet/server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
+while { echo -en "$RESPONSE"; } | ncat -l "${1:-8080}"; do
+  echo "another one falls for the link"
+done
