db1563f7caa62c9ace0bb14cdf6161a7dd1d9e51
SilentProcess <SilentProcess@users.noreply.github.com>
2017-11-22 15:54:09 +0200

    Update server.sh

--- Dokumentointi/Syotteet/server.sh
+++ Dokumentointi/Syotteet/server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# based on Ben Randy's shinatra web server https://github.com/benrady/shinatra
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | ncat -l "${1:-8080}"; do
   echo "another one falls for the link"
